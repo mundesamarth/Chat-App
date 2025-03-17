46452e8da3261b29766058c60f89eb5427ae5af6
Sam <gentalmen28@gmail.com>
2025-03-17 19:47:20 +0000

    Sing in and login setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+import { useEffect } from "react"
 import Chat from "./components/chat/Chat"
 import Detail from "./components/detail/Detail"
 import List from "./components/list/List"
 import Login from "./components/login/Login"
 import Notification from "./components/notification/Notification"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "./Lib/firebase"
+import { useUserStore } from "./Lib/userStore"
 
 const App = () => {
 
-  const user = false
+  const {currentUser,isLoading,fetchUserInfo} = useUserStore();
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user.uid);
+    }
+    )
+    return () =>{
+      unSub();
+    }
+  }, [fetchUserInfo]);
+
+  if(isLoading)return <div className="loading">Loading...</div>
   return (
     <div className='container '>
       {
-        user ? ( 
+        currentUser ? ( 
           <>
           <List/>
           <Chat/>
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify";
-import {createUserWithEmailAndPassword , } from "firebase/auth"
+import {createUserWithEmailAndPassword , signInWithEmailAndPassword } from "firebase/auth"
 import {auth, db} from "../../Lib/firebase"
 import { doc, setDoc } from "firebase/firestore";
 import upload from "../../Lib/upload";
@@ -24,8 +24,23 @@ const Login = () => {
         }
     }
 
-    const handleLogin = e => {
+    const handleLogin = async (e) => {
         e.preventDefault()
+        setLoading(true)
+        const formData = new FormData(e.target)   
+
+        const {email,password} = Object.fromEntries(formData);
+        try{
+          await signInWithEmailAndPassword(auth,email,password)
+        }
+        catch(err){
+          console.log(err)
+          toast.error(err.message)
+        }
+        finally{
+          setLoading(false)   
+      }
+
     }
 
     const handleRegister = async (e) => {
