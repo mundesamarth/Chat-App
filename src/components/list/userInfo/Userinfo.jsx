5041d612c8884da4f77b4d41a6c19a1e1c292988
Sam <gentalmen28@gmail.com>
2025-03-17 14:49:05 +0000

    Chatlist on the left is done

---
+++ src/components/list/userInfo/Userinfo.jsx
@@ -0,0 +1,20 @@
+import "./userInfo.css"
+
+const Userinfo = () => {
+  return (
+    <div className='userInfo'>
+        <div className="user">
+            <img src="./avatar.png" alt="" />
+            <h2>Sam Doe</h2>
+        </div>
+        <div className="icons">
+            <img src="./more.png" alt="more"/>
+            <img src="./video.png" alt="more"/>
+            <img src="./edit.png" alt="more"/>
+
+        </div>
+    </div>
+  )
+}
+
+export default Userinfo
